b68c87fabc6e80882dd448296b23ad05f1e95622
Aleksandra Pawlik <aleksandra.pawlik@alfasystems.com>
2020-05-19 15:47:50 +0100

    test commit

---
+++ src/TripPlanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TripPlanner {
+
+  public static void Main (String[] args) {
+
+  }
+
+  public static void intro() {
+    Scanner input = new Scanner(System.in);
+    //takes user name and destination
+  }
+
+}
+
